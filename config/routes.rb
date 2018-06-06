a892914fa6297e41d27c3dda786392e3944fdc45
Uthaeus <romanlavery@gmail.com>
2018-06-06 15:18:15 -0600

    Installed devise for user authentication and jquery-rails, bootstrap, and cocoon gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#home'
+
+  get 'pages/discussion'
+
   resources :schedules
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
