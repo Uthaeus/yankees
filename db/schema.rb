d57afc52f0e27f24d619f3df07e9b04c3e4e5e93
Uthaeus <romanlavery@gmail.com>
2018-06-07 14:44:08 -0600

    Created show, edit, index, and new views for topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607200220) do
+ActiveRecord::Schema.define(version: 20180607203056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20180607200220) do
     t.string "subtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
