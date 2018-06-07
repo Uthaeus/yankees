47f9890348e7920c3e1ca903474d0b35af3fc555
Uthaeus <romanlavery@gmail.com>
2018-06-07 14:30:23 -0600

    Built out topics controller and added routes and began topics forms

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,63 @@
 class TopicsController < ApplicationController
-  def 
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @topics = Topic.all  
   end
 
-  def 
+  def show
+    @comment = Comment.new
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'Blog was successfully created.' }
+        format.json { render :show, status: :created, location: @topic }
+      else
+        format.html { render :new }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @topic.update(blog_params)
+        format.html { redirect_to @topic, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @topic }
+      else
+        format.html { render :edit }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Topic was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:title, :subtitle)
+    end
+
 end
